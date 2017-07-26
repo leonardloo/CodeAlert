f8071b02cde82f660330b6ab1e93d1f706e99d1c
User <leonard.loo.ks@gmail.com>
2017-07-26 21:59:56 +0800

    Fixed bugs in CodeAlert pingd

--- python/codealert/CodeAlert.py
+++ python/codealert/CodeAlert.py
@@ -1,7 +1,56 @@
 class CodeAlert:
-		
-	# Used for method chaining
+	"""
+	CodeAlert class that sends you a Slack/email/sound notification 
+	when ping() is called.
+
+	"""
+			
+	def __init__(self):
+		"""
+		Init method that does not take in any parameters.
+
+		Parameters
+		----------
+		None
+
+		Returns
+		-------
+		None
+		"""
+
+		################################################
+		# Change this to your self hosted server IP, 
+		# where your node.js server resides. 
+		# A node server is only needed if you require 
+		# email functionality.
+		self.BASE_URL = 'http://52.207.228.129:3001'
+		################################################
+
+		self.ENABLED = True
+		self.SOUND_ENABLED = True
+		self.sound_word = ""
+		self.EMAIL_ENABLED = False
+		self.emails = []
+		self.SLACK_ENABLED = False
+		self.slack_urls = []
+
+		self.logtext = "Your code has finished running!"
+		self.hasError = False
+
 	def set(self, option, val):
+		"""
+		Setter used for method chaining to set options
+
+		Parameters
+		----------
+		option: String key of option
+		val: Object
+
+		Returns
+		-------
+		None
+		"""
+
 		if option == 'sound_enabled':
 			if isinstance(val, bool):
 				self.SOUND_ENABLED = val
@@ -34,27 +83,18 @@ class CodeAlert:
 				self.logtext = val
 		return self
 			
-	def __init__(self):
-		################################################
-		# Change this to your self hosted server IP, 
-		# where your node.js server resides. 
-		# A node server is only needed if you require 
-		# email functionality.
-		self.BASE_URL = 'http://52.207.228.129:3001'
-		################################################
-
-		self.ENABLED = True
-		self.SOUND_ENABLED = True
-		self.sound_word = ""
-		self.EMAIL_ENABLED = False
-		self.emails = []
-		self.SLACK_ENABLED = False
-		self.slack_urls = []
-
-		self.logtext = "Your code has finished running!"
-		self.hasError = False
-			
 	def options(self):
+		"""
+		Getter that returns dictionary of instance options
+
+		Parameters
+		----------
+		None
+
+		Returns
+		-------
+		Dictionary of options
+		"""
 		return {'on': self.ENABLED, 
 						'sound_enabled': self.SOUND_ENABLED, 'sound_word': self.sound_word,
 						'email_enabled': self.EMAIL_ENABLED, 'emails': self.emails, 
@@ -62,6 +102,17 @@ class CodeAlert:
 						'logtext': self.logtext}
 	
 	def set_options(self, options):
+		"""
+		Setter used by pingd decorator to set options
+
+		Parameters
+		----------
+		options: Dictionary of options with string key and object value
+
+		Returns
+		-------
+		None
+		"""
 		if 'on' in options:
 			self.ENABLED = options['on']
 		if 'sound_enabled' in options:
@@ -83,6 +134,17 @@ class CodeAlert:
 			self.logtext = options['logtext']
 	
 	def ping(self, logtxt=""):
+		"""
+		Ping function that sends out the notification.
+
+		Parameters
+		----------
+		logtxt: Custom text that is passed on to Slack/email
+
+		Returns
+		-------
+		None
+		"""
 		import os
 		import requests
 		import json
@@ -111,27 +173,61 @@ class CodeAlert:
 			email_url = 'email'
 			final_url="{0}/{1}".format(self.BASE_URL, email_url)
 			for email in self.emails:
-				payload = {'email': email, 'logtext': logtext_to_use}
-				headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-				response = requests.post(final_url, headers=headers, data=json.dumps(payload))
+				payload = {
+				'email': email, 
+				'logtext': logtext_to_use
+				}
+				headers = {
+				'Content-type': 'application/json', 
+				'Accept': 'text/plain'
+				}
+				response = requests.post(
+					final_url, 
+					headers=headers, 
+					data=json.dumps(payload))
 
 		if self.SLACK_ENABLED:
 			if len(self.slack_urls) == 0:
-				print('Please enter your Slack url for ping. You may obtain it from https://my.slack.com/services/new/incoming-webhook/')
+				print('Please enter your Slack url for ping. ' + 
+					'You may obtain it from ' + 
+					'https://my.slack.com/services/new/incoming-webhook/')
 				return
 			for slack_url in self.slack_urls:
-				payload = {'text': logtext_to_use, 'username': 'CodeAlert', 'icon_emoji': ':tada:'}
-				headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-				response = requests.post(slack_url, headers=headers, data=json.dumps(payload))
+				payload = {
+				'text': logtext_to_use, 
+				'username': 'CodeAlert', 
+				'icon_emoji': ':tada:'
+				}
+				headers = {
+				'Content-type': 'application/json', 
+				'Accept': 'text/plain'
+				}
+				response = requests.post(
+					slack_url, 
+					headers=headers, 
+					data=json.dumps(payload))
 		 
-# Ping decorator
 def pingd(calert=CodeAlert(), options={}):
+	"""
+	Ping decorator that creates a copy of input CodeAlert and sends the ping 
+	at the end of the function or when an Exception is thrown.
+
+	Parameters
+	----------
+	calert: CodeAlert instance that decorator will use
+	options: Custom options to set for CodeAlert instance
+
+	Returns
+	-------
+	Decorated function's output
+	"""
 	import copy
 	import sys
 	try:
 		from StringIO import StringIO
 	except ImportError:
 		from io import StringIO
+
 	_calert = copy.copy(calert)
 	_calert.set_options(options)
 	def ping_decorator(func):
@@ -141,19 +237,23 @@ def pingd(calert=CodeAlert(), options={}):
 			stdout_ = sys.stdout # Keep track of the previous stdout
 			stream = StringIO()
 			sys.stdout = stream
+			result = None
 
 			try:
-				func(*args, **kwargs) # Execute function here while capturing stdout
+				# Execute function here while capturing stdout
+				result = func(*args, **kwargs) 
 			except Exception as e:
 				_calert.hasError = True
 				logtext_to_use = 'Error:\n======\n' + str(e) + '\n\n'
-				# _calert.ping(logtext_to_use + '\nError: ' + str(e))
 
 			sys.stdout = stdout_ # Restore the previous stdout
 			print_statements = stream.getvalue()
+			print(print_statements)
 			logtext_to_use += 'Print:\n======\n' + print_statements
 
 			_calert.ping(logtext_to_use)
 
+			if result:
+				return result
 		return func_wrapper
 	return ping_decorator
--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const bodyParser = require('body-parser');
-const fs = require('fs');
 const email = require('./email');
 
 const app = express()
